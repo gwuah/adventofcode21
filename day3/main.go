5007feef4006a58c3c7901bd29aa53c11091aa13
gwuah <griffithawuah15@gmail.com>
2021-12-04 14:12:45 +0000

    day3 part 2

--- day3/main.go
+++ day3/main.go
@@ -19,7 +19,14 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(int(part1Return))
+	fmt.Println(int(part1Return), "part1")
+
+	part2Return, err := part2(&items)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(int(part2Return))
+
 }
 
 func part1(report *[]string) (int, error) {
@@ -63,4 +70,73 @@ func part1(report *[]string) (int, error) {
 	}
 
 	return int(decimalEpsilon) * int(decimalGamma), nil
+}
+
+func part2(report *[]string) (int, error) {
+
+	resChan := make(chan int64, 2)
+
+	go func() {
+		o, err := oxygen(*report, 0)
+		if err != nil {
+			log.Fatal(err)
+		}
+		resChan <- o
+	}()
+
+	go func() {
+		o, err := carbondioxide(*report, 0)
+		if err != nil {
+			log.Fatal(err)
+		}
+		resChan <- o
+	}()
+
+	answer := (1)
+	answer *= int(<-resChan)
+	answer *= int(<-resChan)
+
+	close(resChan)
+
+	return answer, nil
+
+}
+
+func oxygen(report []string, position int) (int64, error) {
+	if len(report) == 1 {
+		return strconv.ParseInt(report[0], 2, 64)
+	}
+
+	firstBitGroup := groupByBitInPosition(report, position)
+	if len(firstBitGroup["0"]) > len(firstBitGroup["1"]) {
+		return oxygen(firstBitGroup["0"], position+1)
+	} else if len(firstBitGroup["1"]) > len(firstBitGroup["0"]) {
+		return oxygen(firstBitGroup["1"], position+1)
+	} else {
+		return oxygen(firstBitGroup["1"], position+1)
+	}
+}
+
+func carbondioxide(report []string, position int) (int64, error) {
+	if len(report) == 1 {
+		return strconv.ParseInt(report[0], 2, 64)
+	}
+
+	firstBitGroup := groupByBitInPosition(report, position)
+	if len(firstBitGroup["0"]) < len(firstBitGroup["1"]) {
+		return carbondioxide(firstBitGroup["0"], position+1)
+	} else if len(firstBitGroup["1"]) < len(firstBitGroup["0"]) {
+		return carbondioxide(firstBitGroup["1"], position+1)
+	} else {
+		return carbondioxide(firstBitGroup["0"], position+1)
+	}
+}
+
+func groupByBitInPosition(report []string, position int) map[string][]string {
+	bitInPosition := map[string][]string{}
+	for _, binary := range report {
+		char := string(binary[position])
+		bitInPosition[char] = append(bitInPosition[char], binary)
+	}
+	return bitInPosition
 }
